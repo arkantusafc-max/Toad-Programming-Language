60ade8adf00ca220cc3325ea4b5c616a90a6ee83
Awthu1233 <awthu1233@gmail.com>
2026-09-07 17:04:39 -0300

    Added a LOT of things + TODO

--- lexer.py
+++ lexer.py
@@ -1,6 +1,48 @@
 from enum import Enum, auto
-class token(Enum): #Classe que guarda os tipos de token, se você quiser adicionar é so por "nomedotoken = auto()"
-    ID = auto,
-    NUM = auto()
-def addTk(_t, _v): #Função que adiciona um tipo de token
-    return [_t,_v]
+class t(Enum): #Classe que guarda os tipos de token, se você quiser adicionar é so por "nomedotoken = auto()"
+    ID = auto(),
+    NUM = auto(),
+    UNKNOWN = auto(),
+    ERROR = auto()
+def tokenize(_src): #A função que tokeniza
+    tk = [] #a lista de tokens
+    pos = 0 # o caractere atual (começa do zero)
+    n = len(_src) #tamanho da linha
+    while pos < n:
+        if str.isspace(_src[pos]): #Pula o caractere se for um espaço
+            pos+=1
+            continue
+        elif str.isdigit(_src[pos]): #verifica se o caractere é um numero (0-9)
+            buf = ""
+            dot = False
+            err = False
+            while pos < n and (str.isdigit(_src[pos]) or _src[pos] == '.'):
+                if _src[pos] == '.':
+                    if dot:
+                        err = True
+                        break
+                    dot = True
+                buf+=_src[pos]
+                pos+=1
+            if err:
+                tk.append([t.ERROR, "LEXICAL ERROR"]) #Dá erro quando decimal duplo
+                break
+            else:
+                tk.append([t.NUM, buf])
+                continue
+        elif str.isalnum(_src[pos]): #verifica se é um texto alfanumérico
+            buf = ""
+            while pos < n and (str.isalnum(_src[pos]) or _src[pos] == '_'):
+                buf+=_src[pos]
+                pos+=1
+            tk.append([t.ID, buf])
+            continue
+        else:
+            tk.append([t.UNKNOWN, _src[pos]])
+            break
+    return tk
+def printtk(_tk): #Função básica pra printar os tokens
+    for i in _tk:
+        print(f"[Type: '{i[0].name}', Value: '{i[1]}']")
+#TODO: Adicionar mais tipos de tokens
+#0% feito por ia :fogo::fogo:
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import time
+import lexer
 print; ("TOAD -- The programming language being worked on by 2 people") # hi
 time.sleep(2)
 while 1:
-    print(input("TOAD>>"))
+    str = input("TOAD>>")
+    if str == ".exit":
+        break
+    lexer.printtk(lexer.tokenize(str))
