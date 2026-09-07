0187a7752f6fb3de1071bd45512a9ec67a3d2d66
Awthu1233 <awthu1233@gmail.com>
2026-09-07 14:36:11 -0300

    corrigi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-print "TOAD - The Optimistic Awesome Doer!" # this isnt a placeholder btw this is the real acronym
+print("TOAD - The Optimistic Awesome Doer!") # this isnt a placeholder btw this is the real acronym
 time.sleep(1.5)
 while 1:
     print(input("TOAD>>"))
