b4ca9f5a77ddf166a6062fe372438e0e8041ec94
Awthu1233 <awthu1233@gmail.com>
2026-09-19 21:30:11 -0300

    started a parser && changes

--- lexer.py
+++ lexer.py
@@ -1,12 +1,5 @@
 from enum import Enum, auto
 from colorama import Fore
-class t(Enum): #Classe que guarda os tipos de token, se você quiser adicionar é so por "nomedotoken = auto()"
-    ID = auto(),
-    NUM = auto(),
-    UNKNOWN = auto(),
-    ERROR = auto(),
-    PRINT = auto(),
-    STRING = auto()
 def tokenize(_src): #A função que tokeniza
     tk = [] #a lista de tokens
     pos = 0 # o caractere atual (começa do zero)
@@ -27,21 +20,18 @@ def tokenize(_src): #A função que tokeniza
                     dot = True
                 buf+=_src[pos]
                 pos+=1
-            if err:
-                tk.append([t.ERROR, "LEXICAL ERROR"]) #Dá erro quando decimal duplo
+            if err or buf[-1] == ".":
+                tk.append(["ERROR", f"LEXICAL ERROR AT COLUMN {pos+1}"]) #Dá erro quando decimal duplo
                 break
             else:
-                tk.append([t.NUM, buf])
+                tk.append(["NUMBER", float(buf)])
                 continue
-        elif str.isalnum(_src[pos]): #verifica se é um texto alfanumérico
+        elif str.isalnum(_src[pos]) or _src[pos] == '_': #verifica se é um texto alfanumérico
             buf = ""
             while pos < n and (str.isalnum(_src[pos]) or _src[pos] == '_'):
                 buf+=_src[pos]
                 pos+=1
-            if buf == "print":
-                tk.append([t.PRINT, buf])
-            else:
-                tk.append([t.ID, buf])
+            tk.append(["IDENTIFIER", buf])
             continue
         elif _src[pos] == '\"': #Pula o caractere se for um espaço
             buf =""
@@ -49,15 +39,36 @@ def tokenize(_src): #A função que tokeniza
             while pos < n and _src[pos] != '\"':
                 buf+=_src[pos]
                 pos+=1
-            tk.append([t.STRING, buf])
+            tk.append(["STRING", buf])
             pos+=1
             continue
+        elif _src[pos] == '=':
+            tk.append(["EQUALS", '='])
+            pos+=1
+        elif _src[pos] == '+':
+            tk.append(["SUM", '+'])
+            pos+=1
+        elif _src[pos] == '-':
+            tk.append(["MINUS", '-'])
+            pos+=1
+        elif _src[pos] == '/':
+            tk.append(["DIVISION", '/'])
+            pos+=1
+        elif _src[pos] == '$':
+            tk.append(["VAR", '$'])
+            pos+=1
+        elif _src[pos] == '(':
+            tk.append(["LEFT PARENTHESIS", '('])
+            pos+=1
+        elif _src[pos] == ')':
+            tk.append(["RIGHT PARENTHESIS", ')'])
+            pos+=1
         else:
-            tk.append([t.UNKNOWN, _src[pos]])
+            tk.append(["UNKNOWN", _src[pos]])
             break
     return tk
 def printtk(_tk): #Função básica pra printar os tokens
     for i in _tk:
-        print(Fore.GREEN+f"[Type: '{i[0].name}', Value: '{i[1]}']")
+        print(Fore.GREEN+f"[Type: '{i[0]}', Value: '{i[1]}']")
 #TODO: Adicionar mais tipos de tokens
 #0% feito por ia :fogo::fogo:
